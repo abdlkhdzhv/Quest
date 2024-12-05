fdc9307d9d4ef66c8f13dc7ecb87a6b114354472
Rizvan <rizvan@MacBook-Air-Rizvan.local>
2024-12-05 10:54:18 +0300

    добавлен компонент banner

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import store from './redux/store.ts';
+import Banner from './pages/Banner.tsx';
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+import { Provider } from 'react-redux';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <Banner />
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { filterSlice } from './slices/filterSlice';
 
-export const store = configureStore({
+
+const store = configureStore({
   reducer: {
-
+    filters: filterSlice.reducer,
   },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
